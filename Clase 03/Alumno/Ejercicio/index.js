0f3bcc36ef29f87604512be517a603d280533b30
Mariano Bruno <brunomarianofabricio@gmail.com>
2019-10-08 18:25:26 -0300

    Clase 3 (incompleto)

---
+++ Clase 03/Alumno/Ejercicio/index.js
@@ -0,0 +1,68 @@
+
+
+
+var daysOfTheWeek = [
+    'Lunes',
+    'Martes',
+    'Miércoles',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+    'Sábado',
+    'Sabado',
+    'Domingo',
+    'Pato'
+  ]
+
+  for(var i = 0; i < daysOfTheWeek.length; i++){
+    let dia = daysOfTheWeek[i];
+    console.log(dia);
+
+
+
+ 
+switch (dia) {
+
+
+        case 'lunes':
+          console.log('Es un dia habil'); 
+            break;
+
+        case 'Martes':
+            console.log('Es un dia habil');
+            break;
+
+        case 'Miercoles':
+                console.log('Es un dia habil');
+                break;
+
+        
+         case 'Miércoles':
+                console.log('Es un dia habil');
+                break;
+        
+        case 'Jueves':
+                console.log('Es un dia habil');
+                break;
+
+         
+       case 'Viernes':
+                 console.log('Es un dia habil');
+                 break;
+        
+        case 'Sabado':
+            console.log('Es fin de semana');
+            break;
+
+        case 'Domingo':
+            console.log('Es fin de semana');
+            break;
+
+        default:
+            console.log('Dia no hábil');
+            break;
+
+
+}
+
+}
