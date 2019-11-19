349c5cb2ea9acc8c3b871df68ab048565dc637da
Mariano Bruno <brunomarianofabricio@gmail.com>
2019-11-19 20:31:26 -0300

    -
    -

--- Clase 10 - 11/Alumno/Workshop/js/index.js
+++ Clase 10 - 11/Alumno/Workshop/js/index.js
@@ -1,12 +1,30 @@
-let txtprimerNombre = document.getElementById('firstName');
-let txtapellido = document.getElementById('lastName');
-let txtnumeroDocumento = document.getElementById('dni');
-let txtMail = document.getElementById('email');
+var listaAlumnos = [];
 
-txtprimerNombre.addEventListener('input', validarNombre);
-txtnumeroDocumento.addEventListener('input', validarDni);
-txtapellido.addEventListener('input', validarApellido);
-txtMail.addEventListener('input', validarMail);
+//Botones de accion
+let botonAgregar = document.getElementById('btn-add');
+let botonEliminar = document.getElementById('btn-delete');
+let botonBuscar = document.getElementById('btn-search');
+
+//Inputs para dar de alta un nuevo alumno
+let txtprimerNombre = document.getElementById('nombreInput');
+let txtapellido = document.getElementById('apellidoInput');
+let txtnumeroDocumento = document.getElementById('dniInput');
+let txtMail = document.getElementById('emailInput');
+
+//Input para eliminar un alumno
+let txtDniAEliminar = document.getElementById('dniInputEliminar');
+
+//input para buscar un alumno
+let txtNombreABuscar = document.getElementById('txtnombreABuscar');
+
+txtprimerNombre.addEventListener('blur', validarNombre);
+txtnumeroDocumento.addEventListener('blur', validarDni);
+txtapellido.addEventListener('blur', validarApellido);
+txtMail.addEventListener('blur', validarMail);
+
+botonAgregar.addEventListener('click', agregarAlumno);
+botonEliminar.addEventListener('click', eliminarAlumno);
+botonBuscar.addEventListener('click', buscarAlumno);
 
 
  function validarNombre (){
@@ -32,7 +50,7 @@ txtMail.addEventListener('input', validarMail);
     
 
 
-     if(parseInt(dni) < 0){
+     if(dni.length <= 0){
 
          txtnumeroDocumento.classList.add('is-invalid');
     
@@ -77,3 +95,84 @@ txtMail.addEventListener('input', validarMail);
          
 
     }
+
+
+    function agregarAlumno(event) {
+        event.preventDefault();
+
+        var nuevoAlumno = {
+          nombre: txtprimerNombre.value,
+          dni: txtnumeroDocumento.value,
+          apellido: txtapellido.value,
+          mail: txtMail.value
+        };
+      
+        //Agregamos al array el nuevo alumno
+        listaAlumnos.push(nuevoAlumno);
+
+
+        var bodyTable = document.getElementById('bodyTable');
+        var tr = document.createElement('tr');
+        tr.id = txtnumeroDocumento.value;
+        var tdNombre = document.createElement('td');
+        tdNombre.innerHTML = txtprimerNombre.value;
+        tr.appendChild(tdNombre);
+        var tdApellido = document.createElement('td');
+        tdApellido.innerHTML = txtapellido.value;
+        tr.appendChild(tdApellido);
+        var tdDni = document.createElement('td');
+        tdDni.innerHTML = txtnumeroDocumento.value;
+        tr.appendChild(tdDni);
+        var tdEmail = document.createElement('td');
+        tdEmail.innerHTML = txtMail.value;
+        tr.appendChild(tdEmail);
+        bodyTable.appendChild(tr);
+
+        setLocalList('lista',listaAlumnos)
+
+      }
+      
+
+      function setLocalList(key,value){
+        
+        localStorage.setItem(key,JSON.stringify(value))
+
+      }
+
+      function eliminarAlumno(event) {
+        let dni = txtDniAEliminar.value;
+
+        let posicion;
+
+        for (let index = 0; index < listaAlumnos.length; index++) {
+            const alumno = listaAlumnos[index];
+            if(alumno.dni === dni){
+                posicion = index;
+                break
+            }
+        }
+        listaAlumnos.splice(posicion,1);
+        setLocalList('lista',listaAlumnos);
+
+        let tr = document.getElementById(dni);
+        tr.parentNode.removeChild(tr);
+      }
+
+      function buscarAlumno(event) {
+          event.preventDefault();
+
+        let nombre = txtNombreABuscar.value;
+
+        for (let index = 0; index < listaAlumnos.length; index++) {
+            const alumno = listaAlumnos[index];
+            if(nombre === alumno.nombre){
+                document.getElementById('imprimirNombreBusqueda').innerHTML = alumno.nombre;
+                document.getElementById('imprimirApellidoBusqueda').innerHTML = alumno.apellido;
+                document.getElementById('imprimirDniBusqueda').innerHTML = alumno.dni;
+                document.getElementById('imprimirMailBusqueda').innerHTML = alumno.mail;
+
+            }
+        }
+
+    }
+
